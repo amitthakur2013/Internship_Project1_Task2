ed88824fd9d0ba4fd524f382ca0ac73d15fd6c4d
amitthakur2013 <amitthakur2013@gmail.com>
2020-08-19 00:41:44 +0530

    rearrange

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const pdfTemplate =require('../documents');
 
 const storage = multer.diskStorage({
 	destination: function(req,file,cb) {
-		cb(null, 'uploads/');
+		cb(null, 'public/uploads/');
 	},
 	filename:function(req,file,cb) {
 		cb(null,"demo");
